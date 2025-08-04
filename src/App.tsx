c9f5d797401d8b828ae8f6b555065afdf48b6fbd
unknown <guruhari86572@gmail.com>
2025-08-04 19:11:58 +0530

    initial commitaa

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,7 @@
-import { useState } from "react";
 import "./App.css";
 import ExpenceTracker from "./components/ExpenceTracker";
 
 function App() {
-  const [count, setCount] = useState(0);
-
   return (
     <>
       <div className="h-screen flex-col flex justify-center items-center w-full pt-20">
